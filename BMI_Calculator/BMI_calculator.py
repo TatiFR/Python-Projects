d5b722e2ba746c26e0b22623b7952c95564813f6
Tatiana <137820776+TatiFR@users.noreply.github.com>
2024-05-15 16:04:24 +0200

    Delete BMI_Calculator directory

--- BMI_Calculator/BMI_calculator.py
+++
@@ -1,8 +0,0 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-
-f_height = float(height)
-f_weight = float(weight)
-bmi = f_weight / (f_height * f_height)
-i_bmi = int(bmi)
-print(i_bmi)
