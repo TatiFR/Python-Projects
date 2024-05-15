2a4afa82ac28da6f823a2c685ad360c0f71cc5bc
Tatiana <137820776+TatiFR@users.noreply.github.com>
2024-05-15 16:37:05 +0200

    Create main.py

---
+++ Intermediate_Project/17_Quiz_Game/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the Quiz")
+print(f"You're final score was: {quiz.score}/{len(question_bank)}")
